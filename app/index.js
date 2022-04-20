4c26bd7d9235c9681551a109cf7346464a081827
Jesús Martínez <jesus.a.martinez.v@gmail.com>
2022-04-20 15:12:34 -0500

    Initial commit.

---
+++ app/index.js
@@ -0,0 +1,6 @@
+const Web3 = require('web3');
+//let web3 = new Web3('ws://localhost:9545');
+let web3 = new Web3('ws://localhost:7545');
+web3.eth.getChainId().then(function (chainId) {
+    console.log(chainId);
+});
